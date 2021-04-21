816950c6fae6f178b0481931bf904753e6b01433
happy-boksil2 <happyBoksil2@gmail.com>
2021-04-21 22:19:40 +0900

    async, await 실습

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,55 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <button @click="loginUser1">Login</button>
+    <h1>List</h1>
+    <ul>
+    <li v-for="item in items" v-bind:key="item">{{item}}</li>
+  </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
+import { handleException }  from './utils/handler.js';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      items: [],
+    }
+  },
+  methods: {
+    loginUser() {
+      axios.get('https://jsonplaceholder.typicode.com/users/1')
+        .then(response => {
+          if(response.data.id === 1) {
+            console.log('사용자가 인증되었습니다.');
+            axios.get('https://jsonplaceholder.typicode.com/todos')
+              .then(response => {
+                this.items = response.data;
+              });
+          }
+        })
+        .catch(error => console.log(error));
+    },
+    async loginUser1() {
+      try {
+        var response = await axios.get('https://jsonplaceholder.typicode.com/users/1');
+        if(response.data.id === 1) {
+          console.log('사용자가 인증되었습니다.');
+          var list = await axios.get('https://jsonplaceholder.typicode.com/todos');
+          this.items = list.data;
+        }
+      } catch (error) {
+        handleException(error);
+        console.log(error);
+      }
+      
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
